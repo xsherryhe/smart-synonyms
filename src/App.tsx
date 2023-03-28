7a93c66b09e6f5d2fa53ef7156cde1eec5517d29
Sherry He <xsherryhe@gmail.com>
2023-03-27 20:53:39 -0400

    Write basic client-side routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Glosses from './components/Glosses';
+import Home from './components/Home';
+import Synonyms from './components/Synonyms';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/:word" element={<Glosses />} />
+          <Route path="/:pos/:posOffset/synonyms" element={<Synonyms />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
