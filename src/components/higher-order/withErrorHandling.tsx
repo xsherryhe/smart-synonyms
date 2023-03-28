2bb3a67c1e8d417c9901e7a10e2b039b172fff59
Sherry He <xsherryhe@gmail.com>
2023-03-28 03:55:40 -0400

    Add higher-order component for error handling

---
+++ src/components/higher-order/withErrorHandling.tsx
@@ -0,0 +1,20 @@
+import { ComponentType, useCallback, useState } from 'react';
+import ErrorResponse from '../../interfaces/ErrorResponse';
+
+export interface WithErrorHandlingProps {
+  handleErrors(response: ErrorResponse): void;
+}
+
+export function withErrorHandling<P>(ComponentBase: ComponentType<P>) {
+  return function Component(props: Omit<P, 'handleErrors'>) {
+    const [error, setError] = useState<string | null>(null);
+
+    const handleErrors = useCallback(function (response: ErrorResponse) {
+      setError(response.data.error);
+    }, []);
+
+    if (error) return <div className="error">{error}</div>;
+
+    return <ComponentBase handleErrors={handleErrors} {...(props as P)} />;
+  };
+}
