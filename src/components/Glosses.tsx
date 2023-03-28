0867ecb6a12a7948a183a97134f3b91cbdd23df9
Sherry He <xsherryhe@gmail.com>
2023-03-27 21:08:16 -0400

    Add temporary dummy text for other routes

--- src/components/Glosses.tsx
+++ src/components/Glosses.tsx
@@ -1,3 +1,3 @@
 export default function Glosses() {
-  return <></>;
+  return <>Glosses Page</>;
 }
--- src/components/Synonyms.tsx
+++ src/components/Synonyms.tsx
@@ -1,3 +1,3 @@
 export default function Synonyms() {
-  return <></>;
+  return <>Synonyms Page</>;
 }
