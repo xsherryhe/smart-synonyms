d3abd7eb4e74d3e8779e72faa2bdc328d565881b
Sherry He <xsherryhe@gmail.com>
2023-04-03 15:28:49 -0400

    Restructure Glosses component to match new back-end data types

--- src/components/Glosses.tsx
+++ src/components/Glosses.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import fetcher from '../fetcher';
-import GlossesType from '../interfaces/Glosses';
 import partsOfSpeech from '../partsOfSpeech';
+import Word from '../interfaces/Word';
 import {
   withErrorHandling,
   WithErrorHandlingProps,
@@ -13,27 +13,35 @@ interface GlossesBaseProps extends WithErrorHandlingProps {}
 
 function GlossesBase({ handleErrors }: GlossesBaseProps) {
   const { word } = useParams() as { word: string };
-  const [glosses, setGlosses] = useState<GlossesType | null>(null);
+  const [wordData, setWordData] = useState<Word | null>(null);
 
   useEffect(() => {
     async function getGlosses() {
       const response = await fetcher(`words/${word}`);
-      if (response.type === 'success') setGlosses(response.data as GlossesType);
+      if (response.type === 'success') setWordData(response.data as Word);
       else handleErrors(response);
     }
     getGlosses();
   }, [word, handleErrors]);
 
-  if (!glosses) return <div>Loading...</div>;
+  if (!wordData) return <div>Loading...</div>;
   return (
     <>
       <Header searchWord={word} />
-      {glosses.map(({ id, pos, synsets }) => (
+      <h1>{wordData.word}</h1>
+      {wordData.glosses.map(({ id, pos, synsets }) => (
         <div key={id}>
           <h2>{partsOfSpeech[pos]}</h2>
-          {synsets.map(({ pos_offset: posOffset, gloss }) => (
+          {synsets.map(({ pos_offset: posOffset, definition, examples }) => (
             <Link key={posOffset} to={`/${word}/${pos}/${posOffset}/synonyms`}>
-              <button>{gloss}</button>
+              <button>
+                <span>{definition}</span>
+                <div>
+                  {examples.map((example) => (
+                    <span key={example}>{example}</span>
+                  ))}
+                </div>
+              </button>
             </Link>
           ))}
         </div>
