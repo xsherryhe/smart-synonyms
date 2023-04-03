a0bd97f10111913058bdc0a814dc7a52502fd180
Sherry He <xsherryhe@gmail.com>
2023-04-03 17:56:48 -0400

    Style homepage and search bar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,10 @@
+import '../styles/Home.css';
 import Search from './Search';
 
 export default function Home() {
-  return <Search />;
+  return (
+    <main className="home">
+      <Search placeholder="Search a word..." />
+    </main>
+  );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Search.css';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Search({ word = '' }) {
+export default function Search({ word = '', placeholder = 'Search...' }) {
   const [searchValue, setSearchValue] = useState(word);
 
   function updateSearchValue(e: React.ChangeEvent<HTMLInputElement>) {
@@ -9,17 +12,21 @@ export default function Search({ word = '' }) {
   }
 
   return (
-    <>
+    <div className="search">
       <input
         type="text"
         name="search"
         id="search"
         onChange={updateSearchValue}
         value={searchValue}
+        placeholder={placeholder}
+        className="search-input"
       />
       <Link to={`/${searchValue}`}>
-        <button>Search</button>
+        <button className="search-button">
+          <FontAwesomeIcon icon={faSearch} title="Search" />
+        </button>
       </Link>
-    </>
+    </div>
   );
 }
