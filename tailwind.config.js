d0c7e2871ce28bcbc78246c87cc4781faca621ff
Sherry He <xsherryhe@gmail.com>
2023-04-04 15:05:51 -0400

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    colors: {
+      dark: '#9A76A5',
+      'dark-highlight': '#715C8C',
+      light: '#D7D4DD',
+      gray: '#9099A2',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    extend: {
+      fontFamily: {
+        site: [
+          'Montserrat',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Fira Sans',
+          'Droid Sans',
+          'Helvetica Neue',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
