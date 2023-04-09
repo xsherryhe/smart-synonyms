132f8517d1a75739a31769ee73e7098d6c8c4fb0
Sherry He <xsherryhe@gmail.com>
2023-04-09 01:12:33 -0400

    Handle word not found case and refactor into separate components

--- src/components/Glosses.tsx
+++ src/components/Glosses.tsx
@@ -1,61 +1,38 @@
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import fetcher from '../fetcher';
-import partsOfSpeech from '../partsOfSpeech';
 import Word from '../interfaces/Word';
 import {
   withErrorHandling,
   WithErrorHandlingProps,
 } from './higher-order/withErrorHandling';
 import Header from './Header';
+import GlossesMain from './GlossesMain';
 
 interface GlossesBaseProps extends WithErrorHandlingProps {}
 
 function GlossesBase({ handleErrors }: GlossesBaseProps) {
   const { word } = useParams() as { word: string };
-  const [wordData, setWordData] = useState<Word | null>(null);
+  const [wordData, setWordData] = useState<Word | false | null>(null);
 
   useEffect(() => {
     async function getGlosses() {
       const response = await fetcher(`words/${word}`);
-      if (response.type === 'success') setWordData(response.data as Word);
+      if (response.type === 'success')
+        setWordData(response.data as Word | false);
       else handleErrors(response);
     }
     getGlosses();
   }, [word, handleErrors]);
 
-  if (!wordData) return <div>Loading...</div>;
+  let main;
+  if (wordData === null) main = <div>Loading...</div>;
+  else if (wordData === false) main = <div>Word not found!</div>;
+  else main = <GlossesMain word={word} wordData={wordData} />;
   return (
     <>
       <Header searchWord={word} />
-      <h1 className="mt-3 text-center text-4xl font-black leading-tight text-dark">
-        {wordData.word}
-      </h1>
-      <h2 className="mb-3 text-center italic leading-tight text-gray">
-        tap to select definition
-      </h2>
-      {wordData.glosses.map(({ id, pos, synsets }) => (
-        <div
-          key={id}
-          className="relative mx-2 mb-4 mt-6 flex flex-col gap-2.5 rounded-md border-2 border-solid border-light px-2 py-4"
-        >
-          <h3 className="absolute -top-4 left-6 bg-white px-2 font-bold text-dark">
-            {partsOfSpeech[pos]}
-          </h3>
-          {synsets.map(({ pos_offset: posOffset, definition, examples }) => (
-            <Link
-              key={posOffset}
-              to={`/${word}/${pos}/${posOffset}/synonyms`}
-              className="rounded-sm bg-dark text-white hover:bg-dark-highlight"
-            >
-              <button tabIndex={-1}>
-                <div className="m-2 font-semibold">{definition}</div>
-                <div className="m-1 text-sm italic">{examples.join(' • ')}</div>
-              </button>
-            </Link>
-          ))}
-        </div>
-      ))}
+      {main}
     </>
   );
 }
---
+++ src/components/GlossesMain.tsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+import partsOfSpeech from '../partsOfSpeech';
+import Word from '../interfaces/Word';
+
+interface GlossesMainInteface {
+  word: string;
+  wordData: Word;
+}
+
+export default function GlossesMain({ word, wordData }: GlossesMainInteface) {
+  return (
+    <>
+      <h1 className="mt-3 text-center text-4xl font-black leading-tight text-dark">
+        {wordData.word}
+      </h1>
+      <h2 className="mb-3 text-center italic leading-tight text-gray">
+        tap to select definition
+      </h2>
+      {wordData.glosses.map(({ id, pos, synsets }) => (
+        <div
+          key={id}
+          className="relative mx-2 mb-4 mt-6 flex flex-col gap-2.5 rounded-md border-2 border-solid border-light px-2 py-4"
+        >
+          <h3 className="absolute -top-4 left-6 bg-white px-2 font-bold text-dark">
+            {partsOfSpeech[pos]}
+          </h3>
+          {synsets.map(({ pos_offset: posOffset, definition, examples }) => (
+            <Link
+              key={posOffset}
+              to={`/${word}/${pos}/${posOffset}/synonyms`}
+              className="rounded-sm bg-dark text-white hover:bg-dark-highlight"
+            >
+              <button tabIndex={-1}>
+                <div className="m-2 font-semibold">{definition}</div>
+                <div className="m-1 text-sm italic">{examples.join(' • ')}</div>
+              </button>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
