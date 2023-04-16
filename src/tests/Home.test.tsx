24b02a8ce48c0376fd858b80d40f70205a22ecd0
Sherry He <xsherryhe@gmail.com>
2023-04-16 02:54:37 -0400

    Add tests for Home component

---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../components/Home';
+import { RefObject, createRef } from 'react';
+
+jest.mock(
+  '../components/Search',
+  () =>
+    ({
+      resetFocusRef,
+      placeholder,
+    }: {
+      resetFocusRef: RefObject<HTMLElement>;
+      placeholder: string;
+    }) =>
+      <div ref={resetFocusRef as RefObject<HTMLDivElement>}>{placeholder}</div>
+);
+
+describe('Home', () => {
+  describe('structure', () => {
+    it('renders a search bar with the correct placeholder', () => {
+      render(<Home />);
+      const searchBar = screen.getByText('Search a word...');
+      expect(searchBar).toBeInTheDocument();
+    });
+
+    it('renders a search bar with a focus reset ref', () => {
+      const ref = createRef<HTMLElement>();
+      render(<Home resetFocusRef={ref} />);
+      expect(ref.current).not.toBeNull();
+    });
+  });
+});
