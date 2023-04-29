ad6de03b8176036a90787de0e76e00517612a089
Sherry He <xsherryhe@gmail.com>
2023-04-28 21:31:33 -0400

    Correct string typing in utilities file

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,24 +1,24 @@
 import sanitizeHtml from 'sanitize-html';
 
-function stripSlash(string: String) {
+function stripSlash(string: string) {
   return string.replace(/\//g, '');
 }
 
-function spacesToUnderscores(string: String) {
+function spacesToUnderscores(string: string) {
   return string.replace(/ /g, '_');
 }
 
-function underscoresToSpaces(string: String) {
+function underscoresToSpaces(string: string) {
   return string.replace(/_/g, ' ');
 }
 
-export function sanitizeInput(string: String) {
+export function sanitizeInput(string: string) {
   return sanitizeHtml(spacesToUnderscores(stripSlash(string)), {
     allowedTags: [],
     allowedAttributes: {},
   });
 }
 
-export function displayInput(string: String) {
+export function displayInput(string: string) {
   return underscoresToSpaces(string);
 }
